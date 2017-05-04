b7f164a4e992b3d349d881cb1202c2905a6adea8
PRABHAV GARG <prabhav.garg.boss@gmail.com>
2017-05-04 15:35:41 +0530

    Added animation to splash screen

---
+++ app/src/main/java/com/example/boss/learn/Splashs.java
@@ -0,0 +1,81 @@
+package com.example.boss.learn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Splashs extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        RelativeLayout l=(RelativeLayout) findViewById(R.id.relativelayout);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        TextView t1 = (TextView)findViewById(R.id.textView1);
+        TextView t2 = (TextView)findViewById(R.id.textView2);
+        TextView t3 = (TextView)findViewById(R.id.textView3);
+        TextView t4 = (TextView)findViewById(R.id.textView4);
+        ImageView iv = (ImageView) findViewById(R.id.image_splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        t1.clearAnimation();
+        t1.startAnimation(anim);
+        t2.clearAnimation();
+        t2.startAnimation(anim);
+        t3.clearAnimation();
+        t3.startAnimation(anim);
+        t4.clearAnimation();
+        t4.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splashs.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splashs.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splashs.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
+}
